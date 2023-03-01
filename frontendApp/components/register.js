e53ef0521b33a75cc9f20fb11b785bb366fb9ba5
Eryk-Szymanski <e.szymanski.33@interia.pl>
2023-03-01 11:43:44 +0100

    hashing

--- frontendApp/components/register.js
+++ frontendApp/components/register.js
@@ -1,11 +1,28 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Pressable, TextInput } from "react-native";
 import { styles } from "../style/mainStyle";
+var bcrypt = require("bcryptjs");
 
 const RegisterScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [conPassword, setConPassword] = useState("");
+
+  const HandleSubmit = () => {
+    var hash = bcrypt.hashSync(password, 8);
+    console.log(hash);
+    fetch("http://127.0.0.1:8000/users/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: hash,
+      }),
+    });
+  };
 
   return (
     <View style={styles.cardContainer}>
@@ -31,7 +48,7 @@ const RegisterScreen = ({ navigation }) => {
           placeholder="Confirm Password"
           secureTextEntry={true}
         ></TextInput>
-        <Pressable style={styles.button} onPress={() => console.log("lol")}>
+        <Pressable style={styles.button} onPress={() => HandleSubmit()}>
           <Text style={{ color: "#ffffff" }}>Submit</Text>
         </Pressable>
         <Pressable onPress={() => navigation.navigate("Login")}>
